b926d9dffc4c01b5ec8e6fef6decdb2fcd25c85c
flow1981 <contact.f.stagliano@gmail.com>
2020-01-09 10:05:00 +0000

    added main display component

---
+++ src/containers/MainDisplay.js
@@ -0,0 +1,17 @@
+import React,{ Component } from 'react'
+
+import ThreeDisplay from './ThreeDisplay'
+
+class MainDisplay extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <ThreeDisplay />
+         );
+    }
+}
+ 
+export default MainDisplay;
