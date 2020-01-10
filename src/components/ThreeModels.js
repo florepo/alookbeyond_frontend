f74723be0f7bb79986f0d6eff5c428a254492456
flow1981 <contact.f.stagliano@gmail.com>
2020-01-10 20:01:26 +0000

    fixed add and remove to list function

--- src/components/ThreeModels.js
+++ src/components/ThreeModels.js
@@ -2,17 +2,55 @@
 import * as THREE from "three";
 
 export const SatelliteGeoModel = (name="unknown", scaleFactor=0.1, color="#466F81")=> {
+
+    const identifier = name
+
     let geometry = new THREE.BoxGeometry(scaleFactor, scaleFactor, scaleFactor);
+    geometry.name=identifier
+
     let material = new THREE.MeshBasicMaterial({ color});
+    material.name=identifier
+
     let cube = new THREE.Mesh(geometry, material);
-    cube.name= name
+    cube.name= identifier
+
     return cube;
 }
 
 export const EarthGeoModel = (scaleFactor=0.5, color = "#433F81")=> {
+
+    const identifier ="earth"
+
     let geometry = new THREE.SphereGeometry(scaleFactor, 32, 32);
+    geometry.name = identifier
+
     let material = new THREE.MeshBasicMaterial({ color});
-    let sphere = new THREE.Mesh(geometry, material);
-    sphere.name ="earth"
+    material.name = identifier
+
+    let loader = new THREE.TextureLoader()
+    loader.name = identifier
+    // let earthMap = loader.load("surface.jpg")
+    let earthMap = loader.load("https://stemkoski.github.io/AR-Examples/images/earth-sphere.jpg")
+
+    console.log(earthMap)
+    let material2 = new THREE.MeshPhongMaterial({
+        map: earthMap,
+        opacity: 0.5
+      });
+    
+    let sphere = new THREE.Mesh(geometry, material2)
+
+    sphere.name =identifier
+
     return sphere;
+}
+
+export const AmbientLight = () =>{
+
+    const  identifier ="ambient light"
+
+    let ambientLight = new THREE.AmbientLight(0xcccccc, 0.8);
+    ambientLight.name = identifier
+    
+    return ambientLight
 }
--- src/containers/ThreeScene.js
+++ src/containers/ThreeScene.js
@@ -1,15 +1,15 @@
 import React, { Component, Suspense } from "react";
 import * as THREE from "three";
-import {isEqual, differenceBy} from 'lodash'
+import {differenceBy} from 'lodash'
 
-import { SatelliteGeoModel, EarthGeoModel } from "../components/ThreeModels";
+import { SatelliteGeoModel, EarthGeoModel, AmbientLight} from "../components/ThreeModels";
+
 import { intializeSatObject } from "../utils/sathelper.js";
-
 import { sgp4, twoline2satrec, propagate, gstime } from "satellite.js";
 
 //Geometry control parameters
 let scaleFactor = 1 / 4000;
-let satScaleFactor = 1 / 16;
+let satScaleFactor = 0.6;
 
 class ThreeScene extends Component {
   constructor(props) {
@@ -27,7 +27,7 @@ class ThreeScene extends Component {
 
     //ADD CAMERA
     this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-    this.camera.position.z = 4;
+    this.camera.position.z = 3;
 
     //ADD RENDERER
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -35,24 +35,36 @@ class ThreeScene extends Component {
     this.renderer.setSize(width, height);
     this.mount.appendChild(this.renderer.domElement);
 
+    //ADD LIGHTSOURCE
+    let ambientLight = AmbientLight()
+    this.scene.add(ambientLight);       
+    this.state.removable_items.push(ambientLight)   //tracking for garbage collection
+    
     //Add Earth
     let earth = EarthGeoModel(1000*scaleFactor)
     this.scene.add(earth);       
-    this.state.removable_items.push(earth)   //track for garbage collection
+    this.state.removable_items.push(earth)   //tracking for garbage collection
 
-    //dynamically create and add satellite objects based on selection
-    this.props.sats.map(sat => {
-      let satGeoModel = SatelliteGeoModel(sat.name, scaleFactor*500)
-      let satObject = intializeSatObject(sat.name, sat.TLE1, sat.TLE2, satGeoModel, scaleFactor)
-      this.scene.add(satObject);
-      this.state.removable_items.push(satObject)   //track for garbage collection
-    });
+    //ADD Satellites
+    // this.props.sats.map(sat => {
+    //   let satGeoModel = SatelliteGeoModel(sat.name, scaleFactor*500)
+    //   let satObject = intializeSatObject(sat.name, sat.TLE1, sat.TLE2, satGeoModel, scaleFactor)
+
+    //   this.scene.add(satObject);
+    //   this.state.removable_items.push(satObject)   //tracking for garbage collection
+    // });
 
     this.start();
   }
 
   componentWillUnmount() {
     this.stop();
+
+    this.state.removable_items.forEach(element=>{
+      let entity = this.scene.getObjectByName(element.name)
+      this.removeEntityfromScene(entity) //remove from scene
+      this.removeEntityFromMemory(entity) //remove from memory (using .dispose)
+    })
 
     this.mount.removeChild(this.renderer.domElement);
   }
@@ -62,33 +74,48 @@ class ThreeScene extends Component {
 
     //handle removed elements
     if (prevProps.sats.length>this.props.sats.length){
-
-      let removedElements= differenceBy(prevProps.sats, this.props.sats)
-
+      console.log("handle removign elements")
+      const removedElements= differenceBy(prevProps.sats, this.props.sats)
+      console.log("removed",removedElements)
       removedElements.forEach(element=>{
         let entity = this.scene.getObjectByName(element.name)
-        this.removeEntityfromScene(entity) //remove from scene
-        this.removeEntityFromMemory(entity) //remove from memory
+        this.removeEntityfromScene(entity)
+        this.removeEntityFromMemory(entity) //remove from memory (using .dispose)
+
+        if (prevState==this.state) { this.removeItemfromList(element)}
+       
       })
      
     //handle added elements
     } else if (prevProps.sats.length<this.props.sats.length){
-      console.log(prevProps.sats-this.props.sats)
-      console.log("adding not implemented yet")
+      console.log("handling adding elements")
+      const addedElements= differenceBy(this.props.sats,prevProps.sats)
+      console.log("added",addedElements)
+      //dynamically create and add satellite objects based on selection
+      addedElements.forEach(sat => {
+        let satGeoModel = SatelliteGeoModel(sat.name, scaleFactor*500)
+        let satObject = intializeSatObject(sat.name, sat.TLE1, sat.TLE2, satGeoModel, scaleFactor)
+
+        this.scene.add(satObject);
+        this.state.removable_items.push(satObject)   //tracking for garbage collection
+      });
 
     } else
     { console.log("equal")}
-
-
-
+    this.renderScene()
   }
 
-
-
+  removeItemfromList = (entity) =>{
+    console.log("entity to be removed from list:",entity.name)
+    const currentItems = [...this.state.removable_items]
+    const newItems = currentItems.filter(item=>{return item.name!=entity.name})
+    this.setState({removable_items: newItems})
+  }
 
   removeEntityfromScene = (entity) => {
+    console.log("entity to be removed:",entity.name)
     this.scene.remove( entity );
-    this.renderScene();
+    this.renderer.render(this.scene, this.camera);
   }
 
   removeEntityFromMemory =(entity) =>{
@@ -117,30 +144,6 @@ class ThreeScene extends Component {
   renderScene = () => {
     this.renderer.render(this.scene, this.camera);
   };
-
-
-
-
-
-  createSatelliteObject(name, tle1, tle2) {
-    let TimeSinceEpoch = 0; //[min]
-
-    let sat = SatelliteGeoModel();  //get three.js geometry model
-    console.log('old',sat)
-    let satrec = twoline2satrec(tle1, tle2);
-    let positioAndVelocity = sgp4(satrec, TimeSinceEpoch);
-
-    sat.position.set(
-      positioAndVelocity.position.x * scaleFactor, //map ECI to ThreeJS coord system > x(js) = z(ECI), y(js) = x(ECI), z(js)=y(ECI)
-      positioAndVelocity.position.y * scaleFactor,
-      positioAndVelocity.position.z * scaleFactor
-    );
-
-    sat.userData.satrec = satrec;
-    sat.userData.name = name;
-
-    return sat;
-  }
 
 
   render() {
