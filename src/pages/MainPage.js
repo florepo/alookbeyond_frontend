1c53377edaa992982248e6d2871cf51342f7ce86
flow1981 <contact.f.stagliano@gmail.com>
2020-02-03 14:09:33 +0800

    simplified main-page structure

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,40 +1,16 @@
-import React, { Component } from 'react'
-
+import React from 'react'
 import NavBar from '../containers/NavBar'
 import MainDisplay from '../containers/MainDisplay'
-import Footer from '../containers/Footer'
 
-import * as API from '../adapters/api'
+const MainPage = () => {
 
-class MainPage extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {loadedView: [],
-                     currentView: []
-                    }
-      }
-
-    handleItemClick = (id) => {
-        console.log("watchlist clicked", id)
-        API.getWatchlist(id).then(watchlist => this.setState({loadedView: watchlist}))
-    }
-
-
-    render() { 
-        return (
-            <div>
-                <NavBar className="navbar"
-                   onClick={this.handleItemClick}
-                />
-                <MainDisplay
-                    className="main-display"
-                    loadedView={this.state.loadedView}
-                    addSatToWatchList={this.addToWatchlist}  
-                    removeSat={this.removeFromWatchlist} 
-                />
-            </div>
-        )
-    }
+    return (
+        <div className = "main-page" >
+            <NavBar className = "navbar" />
+            <MainDisplay className = "main-display" />
+        </div>
+    )
+    
 }
  
 export default MainPage;
